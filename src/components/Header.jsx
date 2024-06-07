1542f37620a743d675ecf978c20b2a84edddb66b
Jittapol Srisawang <147258184+JittapolSrisawang@users.noreply.github.com>
2024-06-07 11:46:45 +0700

    add NextAuth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -32,7 +37,23 @@ export default function Header() {
         />
 
         {/* menu item */}
-        <button className="text-sm font-semibold text-blue-500">Log In</button>
+
+        {session ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={session.user.image}
+            alt={session.user.name}
+            className="h-10 w-10 rounded-full cursor-pointer"
+            onClick={signOut}
+          />
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
+        )}
       </div>
     </div>
   );
